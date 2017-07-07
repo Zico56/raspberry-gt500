112f19cf8d54cb48a66de50a73c04d18d8ddaa08
Erwan.C <ecozic-prest@redoute.fr>
2017-07-07 15:49:27 +0200

    code refactoring

--- Application.py
+++ Application.py
@@ -1,17 +1,17 @@
 import logging
 from tkinter import *
-#import gobject
-#import gst
 
 logging.basicConfig(format='%(asctime)s : %(message)s', datefmt='%d/%m/%Y %H:%M:%S', filename='application.log', level=logging.DEBUG)
 
-ledsState = {}
+gridNumberOfColumns = 5
+indicatorImgList = [
+    "gif/rsz_voyant_phare1_off.png",
+    "gif/rsz_voyant_phare2_off.png",
+    "gif/rsz_voyant_phare3_off.png",
+    "gif/rsz_voyant_warning_off.png",
+    "gif/rsz_voyant_moteur_off.png"]
 
-'''
-def callbackButton(event):
-    #print("Button clicked: ", event.widget['text'])
-    logging.info("Button clicked: "+event.widget['text'])
-'''
+ledsState = {}
 
 def callbackLed(event):
     imgOn = PhotoImage(file="gif/rsz_red-led-on-th.png")
@@ -37,7 +37,6 @@ def initLed(frame):
     ledsState[str(led)] = "OFF"
     return led
     
-    
 fenetre = Tk()
 fenetre.wm_title("Rasperry GT500")
 
@@ -45,7 +44,6 @@ fenetre.wm_title("Rasperry GT500")
 #fenetre.overrideredirect(1)
 
 verticalPW = PanedWindow(fenetre, orient=VERTICAL, bg="black")
-#verticalPW.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
 
 photo = PhotoImage(file="shelby.png")
 canvas = Canvas(width=350, height=200, bg="black", highlightthickness=0)
@@ -54,68 +52,18 @@ verticalPW.add(canvas)
 
 frame = Frame(bg="black", bd=0)
 
-led1 = initLed(frame)
-led1.grid(row=0, column=0)
-
-led2 = initLed(frame)
-led2.grid(row=0, column=1)
-
-led3 = initLed(frame)
-led3.grid(row=0, column=2)
-
-led4 = initLed(frame)
-led4.grid(row=0, column=3)
-
-led5 = initLed(frame)
-led5.grid(row=0, column=4)
-
-imgIndic1 = PhotoImage(file="gif/rsz_voyant_phare1_off.png")
-indicator1 = Label(frame, image=imgIndic1, bg="black")
-indicator1.grid(row=1, column=0)
-
-imgIndic2 = PhotoImage(file="gif/rsz_voyant_phare2_off.png")
-indicator2 = Label(frame, image=imgIndic2, bg="black")
-indicator2.grid(row=1, column=1)
-
-imgIndic3 = PhotoImage(file="gif/rsz_voyant_phare3_off.png")
-indicator3 = Label(frame, image=imgIndic3, bg="black")
-indicator3.grid(row=1, column=2)
-
-imgIndic4 = PhotoImage(file="gif/rsz_voyant_warning_off.png")
-indicator4 = Label(frame, image=imgIndic4, bg="black")
-indicator4.grid(row=1, column=3)
-
-imgIndic5 = PhotoImage(file="gif/rsz_voyant_moteur_off.png")
-indicator5 = Label(frame, image=imgIndic5, bg="black")
-indicator5.grid(row=1, column=4)
-
-'''
-btn1 = Button(frame, text="Btn 1", command="14", padx ="1px", pady="1px", bd="1px", fg="blue", activeforeground="blue")
-btn1.grid(row=1, column=0)
-btn1.bind("<Button-1>", callbackButton)
-
-btn2 = Button(frame, text="Btn 2", command="14", padx ="1px", pady="1px", bd="1px", fg="blue", activeforeground="blue")
-btn2.grid(row=1, column=1)
-btn2.bind("<Button-1>", callbackButton)
-
-btn3 = Button(frame, text="Btn 3", command="14", padx ="1px", pady="1px", bd="1px", fg="blue", activeforeground="blue")
-btn3.grid(row=1, column=2)
-btn3.bind("<Button-1>", callbackButton)
-
-btn4 = Button(frame, text="Btn 4", command="14", padx ="1px", pady="1px", bd="1px", fg="blue", activeforeground="blue")
-btn4.grid(row=1, column=3)
-btn4.bind("<Button-1>", callbackButton)
-
-btn5 = Button(frame, text="Btn 5", command="14", padx ="1px", pady="1px", bd="1px", fg="blue", activeforeground="blue")
-btn5.grid(row=1, column=4)
-btn5.bind("<Button-1>", callbackButton)
-'''
-
-frame.columnconfigure(0, weight=20)
-frame.columnconfigure(1, weight=20)
-frame.columnconfigure(2, weight=20)
-frame.columnconfigure(3, weight=20)
-frame.columnconfigure(4, weight=20)
+for x in range(0, gridNumberOfColumns):
+    #logging.info("We're on time %d" % (x))
+    led = initLed(frame)
+    led.grid(row=0, column=x)
+    
+    #logging.info("img: " + indicatorImgList[x])
+    imgIndic = PhotoImage(file=indicatorImgList[x])
+    indicator = Label(frame, image=imgIndic, bg="black")
+    indicator.image = imgIndic
+    indicator.grid(row=1, column=x)
+    
+    frame.columnconfigure(x, weight=20)
 
 verticalPW.add(frame)
 verticalPW.pack()
