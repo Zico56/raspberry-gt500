8cf89b7f3a825b66d4a389c3cd01ee306abcb0eb
Erwan <Erwan@MacBook-Pro-de-Erwan.local>
2017-07-09 23:07:12 +0200

    adding Led class and use of Pillow

--- Application.py
+++ Application.py
@@ -1,89 +1,53 @@
 import logging
 from tkinter import *
+from PIL import Image, ImageTk, ImageDraw
+from Led import Led
 
 logging.basicConfig(format='%(asctime)s : %(message)s', datefmt='%d/%m/%Y %H:%M:%S', filename='application.log', level=logging.DEBUG)
 
-gridNumberOfColumns = 5
-indicatorImgList = [
-    "gif/rsz_voyant_phare1_off.png",
-    "gif/rsz_voyant_phare2_off.png",
-    "gif/rsz_voyant_phare3_off.png",
-    "gif/rsz_voyant_warning_off.png",
-    "gif/rsz_voyant_moteur_off.png"]
-ledsState = {}
-
-def callbackLed(event):
-    imgOn = PhotoImage(file="gif/rsz_red-led-on-th.png")
-    imgOff = PhotoImage(file="gif/rsz_red-led-off-th.png")
-    
-    state = ledsState[str(event.widget)]
-    if ( state == "OFF"):
-        event.widget.configure(image=imgOn)
-        event.widget.image=imgOn
-        ledsState[str(event.widget)] = "ON"
-    elif ( state == "ON"):
-        event.widget.configure(image=imgOff)
-        event.widget.image=imgOff
-        ledsState[str(event.widget)] = "OFF"
-    else:
-        raise Exception('Unknow led state')
-
-def callback1(event):
-    logging.info("callback 1")
-    callbackLed(event)
-
-def callback2(event):
-    logging.info("callback 2")
-    callbackLed(event)
-
-def callback3(event):
-    logging.info("callback 3")
-    callbackLed(event)
-
-def callback4(event):
-    logging.info("callback 4")
-    callbackLed(event)
-
-def callback5(event):
-    logging.info("callback 5") 
-    callbackLed(event)    
-    
-callbackMethodList = [
-    callback1,
-    callback2,
-    callback3,
-    callback4,
-    callback5]
- 
-def initLed(frame):
-    img = PhotoImage(file="gif/rsz_red-led-off-th.png")
-    led = Label(frame, image=img, bg="black")
-    led.image = img
-    #led.bind("<Button-1>", callbackLed)    
-    ledsState[str(led)] = "OFF"
-    return led
-    
 fenetre = Tk()
-fenetre.wm_title("Rasperry GT500")
 
 #Window without title and border
 #fenetre.overrideredirect(1)
 
+fenetre.wm_title("Rasperry GT500")
+
+gridNumberOfColumns = 5
+ledTab = []
+indicatorImgList = [
+    "gif/rsz_voyant_phare1_off.gif",
+    "gif/rsz_voyant_phare2_off.gif",
+    "gif/rsz_voyant_phare3_off.gif",
+    "gif/rsz_voyant_warning_off.gif",
+    "gif/rsz_voyant_moteur_off.gif"]
+
+def createImage(imgPath):
+    image = Image.open(imgPath)
+    photoImage = ImageTk.PhotoImage(image)
+    return photoImage
+
+#---------------------------------------------------------------------------------
+
 verticalPW = PanedWindow(fenetre, orient=VERTICAL, bg="black")
 
-photo = PhotoImage(file="shelby.png")
+imgBg = createImage("png/shelby.png")
 canvas = Canvas(width=350, height=200, bg="black", highlightthickness=0)
-canvas.create_image(0, 0, anchor=NW, image=photo)
+canvas.create_image(0, 0, anchor=NW, image=imgBg)
 verticalPW.add(canvas)
 
 frame = Frame(bg="black", bd=0)
+'''
+led = Led(frame)
+led.label.grid(row=0, column=0)
+'''
 
 for x in range(0, gridNumberOfColumns):
     #logging.info("We're on time %d" % (x))
-    led = initLed(frame)
-    #callback = func = getattr(root, callbackMethodList[x]) 
-    led.bind("<Button-1>", callbackMethodList[x])
-    led.grid(row=0, column=x)
+    led = Led(frame)
+    ledTab.append(led)
+    
+    #led.bind("<Button-1>", callbackMethodList[x])
+    led.label.grid(row=0, column=x)
     
     #logging.info("img: " + indicatorImgList[x])
     imgIndic = PhotoImage(file=indicatorImgList[x])
@@ -93,10 +57,14 @@ for x in range(0, gridNumberOfColumns):
     
     frame.columnconfigure(x, weight=20)
 
+frame.pack()
+
 verticalPW.add(frame)
 verticalPW.pack()
 
 fenetre.geometry('%dx%d+%d+%d' % (350, 295, 0, 0))
 fenetre.mainloop()
 
+
+    
 
---
+++ Led.py
@@ -0,0 +1,50 @@
+import logging
+from tkinter import *
+from PIL import Image, ImageTk, ImageDraw
+
+logging.basicConfig(format='%(asctime)s : %(message)s', datefmt='%d/%m/%Y %H:%M:%S', filename='application.log', level=logging.DEBUG)
+
+class Led:
+
+    imageOff = Image.open("jpg/rsz_red-led-off-th.jpg")
+    imageOn = Image.open("jpg/rsz_red-led-on-th.jpg")
+
+    def __init__(self, parent):
+        self.state = "OFF"
+        
+        self.img = ImageTk.PhotoImage(self.imageOff)
+        self.label = Label(parent, image=self.img, bg="black")
+        self.label.image = self.img
+        self.label.bind("<Button-1>", self.callback)
+        
+        '''
+        self.image = Image.open("gif/rsz_red-led-off-th.gif")
+        self.photoImage = ImageTk.PhotoImage(self.image)
+        self.canvas = Canvas(parent, width=25, height=25, bg="white", highlightthickness=0)
+        self.canvas.create_image(0, 0, anchor=NW, image=self.photoImage)
+        self.canvas.bind("<Button-1>", self.callback)
+        '''
+
+    def convertPngToJpeg():
+        self.image = Image.open("png/rsz_green-led-on-th.png","r")
+        self.bg = Image.new('RGB', (25,25), (0,0,0))
+        self.text_img = Image.new('RGBA', (25,25), (0, 0, 0, 0))
+        self.text_img.paste(self.bg, (0,0))
+        self.text_img.paste(self.image, (0,0), mask=self.image)       
+        self.text_img.save("jpg/rsz_green-led-on-th.jpg", format="png")
+
+    def callback(self, event):
+        logging.info("callback led")
+        self.changeColor()
+
+    def changeColor(self):
+        if ( self.state == "OFF"):
+            self.img = ImageTk.PhotoImage(self.imageOn)
+            self.label.configure(image=self.img)
+            self.state = "ON"
+        elif ( self.state == "ON"):
+            self.img = ImageTk.PhotoImage(self.imageOff)
+            self.label.configure(image=self.img)
+            self.state = "OFF"
+        else:
+            raise Exception('Unknow led state')
