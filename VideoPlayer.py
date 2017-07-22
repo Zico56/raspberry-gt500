e42fb8ca966f065803c05debe0a4d14dbaf6a61a
Erwan <Erwan@MacBook-Pro-de-Erwan.local>
2017-07-22 15:16:23 +0200

    add clean exit for GPIO

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -1,7 +1,33 @@
 import tkinter as tk
-import threading
-import imageio
-from PIL import Image, ImageTk
+#import threading
+#import imageio
+#from PIL import Image, ImageTk
+import pygame
+
+FPS = 60
+
+pygame.init()
+clock = pygame.time.Clock()
+movie = pygame.movie.Movie('video/sample.mp4')
+screen = pygame.display.set_mode(movie.get_size())
+movie_screen = pygame.Surface(movie.get_size()).convert()
+
+movie.set_display(movie_screen)
+movie.play()
+
+
+playing = True
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            movie.stop()
+            playing = False
+
+    screen.blit(movie_screen,(0,0))
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
 
 '''
 video_name = "video/sample.mp4" #This is your video file path
@@ -29,7 +55,6 @@ if __name__ == "__main__":
     thread.daemon = 1
     thread.start()
     root.mainloop()
-'''
 
 from skvideo.io import VideoCapture
 
@@ -42,3 +67,4 @@ while True:
     # do something with image here
     if not retval:
         break
+'''
